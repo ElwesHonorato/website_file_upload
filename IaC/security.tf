a719b515e084651a401d18177ee03cb59f60951b
Elwes_Honorato <elweshonorato@gmail.com>
2022-10-11 08:46:46 -0300

    ECS

--- IaC/security.tf
+++ IaC/security.tf
@@ -9,15 +9,15 @@ resource "aws_default_vpc" "default_vpc" {
 
 # Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
-  availability_zone = "eu-east-2a"
+  availability_zone = "us-east-2a"
 }
 
 resource "aws_default_subnet" "default_subnet_b" {
-  availability_zone = "eu-east-2b"
+  availability_zone = "us-east-2b"
 }
 
 resource "aws_default_subnet" "default_subnet_c" {
-  availability_zone = "eu-east-2c"
+  availability_zone = "us-east-2c"
 }
 
 # resource "aws_security_group" "permitir_ssh_http" {
